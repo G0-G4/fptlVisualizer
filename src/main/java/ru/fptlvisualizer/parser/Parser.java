c909e15b98aa009acc6f115d300d4da50743505f
George Grishchenkov <g.grishchenkov@hh.ru>
2024-11-05 23:22:00 +0300

    without ternary right operator

--- src/main/java/ru/fptlvisualizer/parser/Parser.java
+++ src/main/java/ru/fptlvisualizer/parser/Parser.java
@@ -1,6 +1,16 @@
 package ru.fptlvisualizer.parser;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 public class Parser {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws ParseException {
+    String code = """
+        a=b->c,d->e,f;
+        """;
+    InputStream stream = new ByteArrayInputStream(code.getBytes());
+    FptlParser parser = new FptlParser(stream);
+    ru.fptlvisualizer.parser.SimpleNode n = parser.Function();
+    n.dump("");
   }
 }
